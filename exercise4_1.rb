12a785a2348c11a0bc996daac9a36a65b02910be
drawsarahb@gmail.com <sbroberts.design@Sarahs-MacBook-Pro.local>
2017-08-08 14:01:47 -0400

    Revised exercise 4_1

--- exercise4_1.rb
+++ exercise4_1.rb
@@ -1,6 +1,7 @@
 number = gets.to_i
 
 if number > 100
-  puts " that's a big number!"
-otherwise
-  puts "why dream a littler bigger?""
+  puts "that's a big number!"
+else
+  puts "why dream a little bigger?"
+end
---
+++ exercise4_2.rb
@@ -0,0 +1,9 @@
+user_age = gets.to_i
+my_age = 30
+
+if user_age > 105
+  puts "I'm not sure I believe you"
+else
+  puts "#{user_age - my_age}"
+
+end
