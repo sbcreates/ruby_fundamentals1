afd211b20ece7dbbf2ce825a2a1ba0476360b599
drawsarahb@gmail.com <sbroberts.design@Sarahs-MacBook-Pro.local>
2017-08-08 15:08:29 -0400

    Completed exercise 5

--- exercise4_5.rb
+++ exercise4_5.rb
@@ -1,1 +1,13 @@
 secret_number = 25
+
+puts "Enter a number"
+
+number = gets.to_i
+
+if number == 25
+  puts "You win!!"
+elsif number == 24 or 26
+  puts "So close!"
+else
+  puts "Try again"
+end
