78af3bbb181130b9703ca6cfde4eb9c67bad2c40
drawsarahb@gmail.com <sbroberts.design@Sarahs-MacBook-Pro.local>
2017-08-08 13:40:23 -0400

    Completed exercise 2 and beginning exercise 3

---
+++ exercise2.rb
@@ -0,0 +1,10 @@
+tip = 0.20
+
+puts 50 * tip
+
+puts (50 + tip).to_s
+
+puts "If we multiply 45628 by 7839, we will get the result #{45628 * 7839}"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+#false || true should equal true
